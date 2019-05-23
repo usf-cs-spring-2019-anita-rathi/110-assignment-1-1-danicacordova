90ecd4adfcc71b76ec8400ce568dfa78665fda0d
Danica Cordova <danicacordova@Danicas-MacBook-Pro.local>
2019-05-23 15:50:56 -0700

    description

---
+++ E1.6.py
@@ -0,0 +1,23 @@
+#Exercise 1.6
+
+#1
+radius=float(input("Enter the radius:"))
+area=float(radius**2*3.14)
+circ=float(radius*2*3.14)
+print("Area:",area)
+print("Circumference:",circ)
+
+#2
+VAR1=float(input("Enter Variable 1:"))
+VAR2=float(input("Enter Variable 2:"))
+print("OG Variable 1:", VAR1)
+print("OG Variable 2:", VAR2)
+NVAR1=VAR2
+NVAR2=VAR1
+print("New Variable 1:", NVAR1)
+print("New Variable 2:", NVAR2)
+
+#3
+celc=float(input("Enter degrees in Celcius:"))
+fheit=float(9/5*celc+32)
+print("Degree(s) in Fahrenheit:",fheit)
